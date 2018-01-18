d7dd62578f0f3cafa2fcb15da68ccaeec8ecb091
ggrafutkin <ggrafutkin@mirantis.com>
2018-01-18 12:50:52 -0800

    error handling, new command 'now', log is now appending

--- parse.py
+++ parse.py
@@ -4,7 +4,7 @@ from random import randint
 from lxml import html
 import os
 import time
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, RegexHandler
 import csv
 
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
@@ -14,7 +14,12 @@ subscribers = set()
 
 
 def request_fees():
-    page = requests.get('https://yobit.net/ru/fees/')
+    try:
+        page = requests.get('https://yobit.net/ru/fees/')
+    except Exception as ex:
+        print 'Failed to fetch the website:', ex.message
+        return {}
+
     tree = html.fromstring(page.content.decode('utf-8'))
 
     in_fee = 0
@@ -36,15 +41,23 @@ def request_fees():
 
 def subscribe(bot, update):
     subscribers.add(update.message.chat_id)
-    update.message.reply_text('You have been subscribed')
+    update.message.reply_text(u'Вы подписаны на обновления')
 
 
 def unsubscribe(bot, update):
     if update.message.chat_id in subscribers:
         subscribers.remove(update.message.chat_id)
-        update.message.reply_text('You have been unsubscribed')
+        update.message.reply_text(u'Вы отписаны от обновлений')
     else:
-        update.message.reply_text('You are not in the list')
+        update.message.reply_text(u'Не обнаружил Вас в подписках')
+
+
+def now(bot, update):
+    fees = request_fees()
+    if fees:
+        update.message.reply_text(u'Текущая комиссия: ввод: {fees[in_fee]}%, вывод: {fees[out_fee]}%'.format(fees=fees))
+    else:
+        update.message.reply_text(u'Возникли проблемы с доступ к вебсайту. Обратитесь в поддержку')
 
 
 def error(bot, update, error):
@@ -58,35 +71,41 @@ def main():
 
     dp.add_handler(CommandHandler('subscribe', subscribe))
     dp.add_handler(CommandHandler('unsubscribe', unsubscribe))
+    dp.add_handler(RegexHandler('now', now))
 
     dp.add_error_handler(error)
 
     updater.start_polling()
     prev_fees = {}
-    with open('export/output.csv', 'wb') as f:
-        writer = csv.writer(f)
-        writer.writerow(('Time', 'In_Fee', 'Out_Fee'))
-        try:
-            while True:
-                fees = request_fees()
-                if fees != prev_fees:
-                    writer.writerow((time.strftime('%c'), fees['in_fee'], fees['out_fee']))
-                    f.flush()
-                    for chat_id in subscribers:
-                        if prev_fees:
-                            msg = u'Комиссия QIWI изменилась. Ввод: {prev_fees[in_fee]}% -> {fees[in_fee]}%,'.format(
-                                prev_fees=prev_fees, fees=fees) + \
-                                  u' вывод: {prev_fees[out_fee]}% -> {fees[out_fee]}%'.format(prev_fees=prev_fees,
-                                                                                              fees=fees)
-                        else:
-                            msg = u'Парсер начал работу. Начальные значения: ' + \
-                                  u'ввод:{fees[in_fee]}%, вывод:{fees[out_fee]}%'.format(fees=fees)
-                        updater.bot.send_message(chat_id=chat_id, text=msg)
-                prev_fees = fees
-                time.sleep(UPDATE_INTERVAL + randint(-5, 5))
-        except KeyboardInterrupt:
-            updater.stop()
-            f.close()
+    try:
+        with open('export/output.csv', 'a') as f:
+            writer = csv.writer(f)
+            try:
+                while True:
+                    fees = request_fees()
+                    if fees and fees != prev_fees:
+                        writer.writerow((time.strftime('%c'), fees['in_fee'], fees['out_fee']))
+                        f.flush()
+                        for chat_id in subscribers:
+                            if prev_fees:
+                                msg = u'Комиссия QIWI изменилась. Ввод: {prev_fees[in_fee]}% -> {fees[in_fee]}%,'.format(
+                                    prev_fees=prev_fees, fees=fees) + \
+                                      u' вывод: {prev_fees[out_fee]}% -> {fees[out_fee]}%'.format(prev_fees=prev_fees,
+                                                                                                  fees=fees)
+                            else:
+                                msg = u'Парсер начал работу. Начальные значения: ' + \
+                                      u'ввод:{fees[in_fee]}%, вывод:{fees[out_fee]}%'.format(fees=fees)
+                            updater.bot.send_message(chat_id=chat_id, text=msg)
+                    prev_fees = fees
+                    time.sleep(UPDATE_INTERVAL + randint(-5, 5))
+            except KeyboardInterrupt:
+                f.close()
+    except IOError as ex:
+        print 'Failed write to the output file:', ex.message
+    finally:
+        for chat_id in subscribers:
+            updater.bot.send_message(chat_id=chat_id, text=u'Завершаю работу... До встречи!')
+        updater.stop()
 
 
 if __name__ == '__main__':
